12ccd97574c7f47565ec267b682c940a23dab147
Yuji HAMADA <yuji.tky.ykhm@gmail.com>
2015-09-19 21:01:26 +0900

    Not show welcome screen

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 ;; Key bind
 (define-key global-map (kbd "C-h") 'delete-backward-char) ; delete
 (define-key global-map (kbd "C-z") 'undo) ; undo
+
+;; Not show welcome screen
+(setq inhibit-startup-message t)
